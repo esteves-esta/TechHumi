1a66865d483b531a0f5d1327fd9cb822360fb897
Vitor Oliveira <39301396+virtohoho@users.noreply.github.com>
2019-03-29 16:56:18 -0300

    Update de Arquivos base

---
+++ Arduino/TechHumi_Analog/TechHumi_Analog.ino
@@ -0,0 +1,30 @@
+#include <dht.h> // Inclui a biblioteca no seu código
+ 
+dht DHT; // Cria um objeto da classe dht
+uint32_t timer = 0;
+ 
+void setup()
+{
+  Serial.begin(9600); // Inicializa serial com taxa de transmissão de 9600 bauds
+}
+ 
+void loop()
+{
+  // Executa 1 vez a cada 2 segundos
+  if(millis() - timer>= 2000)
+  {
+ 
+    DHT.read11(A1); // chama método de leitura da classe dht,
+                    // com o pino de transmissão de dados ligado no pino A1
+ 
+    // Exibe na serial o valor de umidade
+    Serial.print(DHT.humidity);
+    Serial.println(" %");
+ 
+    // Exibe na serial o valor da temperatura
+    Serial.print(DHT.temperature);
+    Serial.println(" Celsius");
+ 
+    timer = millis(); // Atualiza a referência
+  }
+}
