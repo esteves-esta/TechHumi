504ada55ae00467ca5625ad1a84515f21eb606a2
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-05-28 22:26:11 -0300

    Ajustes Banco - @virtohoho

--- banco_de_dados/ScriptBDTechHumiAZURE.sql
+++ banco_de_dados/ScriptBDTechHumiAZURE.sql
@@ -2,8 +2,6 @@
 idEmpresa int primary key identity(1,1),
 nomeEmpresa varchar(60) not null,
 cnpjEmpresa varchar(30) unique,
-nomeRepresentante varchar (60) not null,
-emailRepresentante varchar(80) unique,
 telefoneEmpresa1 varchar(20) unique,
 telefoneEmpresa2 varchar(20) unique
 );
@@ -68,17 +66,34 @@ horaFim time not null,
 fkSensor int foreign key references Sensor(idSensor) 
 );
 
-insert into Endereco(logradouro,numero,bairro,complemento,cidade,uf,cep,referencia) values
-('R. Edmundo Orioli','225','Cidade Tiradentes','44A','São Paulo','SP','08470-600','Escola Camilo'),
-('R. Serra de Botucatu','2095','Vila Carrão','','São Paulo','SP','03317-001','');
+insert into Empresa (nomeEmpresa,cnpjEmpresa,telefoneEmpresa1,telefoneEmpresa2) values
+('TechHumi','28.118.114/0001-50','(11)2516-7236','(11)95366-5905');
 
+insert into Endereco(logradouro,numero,bairro,complemento,cidade,uf,cep,referencia,fkEmpresa) values
+('R. Edmundo Orioli','225','Cidade Tiradentes','44A','São Paulo','SP','08470-600','Escola Camilo',1);
 
 select * from Endereco;
 
-insert into Login (loginUsuario,senhaUsuario,nivelAcesso) values
-('admin','admin',1),
-('usuario','usuario',2);
+insert into Funcionario(nomeFuncionario,rgFuncionario,cpfFuncionario,emailFuncionario,telefoneFuncionario,cargoFuncionario,fkEmpresa) values 
+('Vitor Leonardo Gonçalves de Oliveira Silva','37.481.521-9','480.023.422.192-10','vitor.osilva@bandtec.com.br','(11)92312-3211','Representante',1),
+('Letícia Lago Mori','23.471.373-2','157.954.708-72','leticia.mori@bandtec.com.br','(11)95218-7232','Desenvolvedor Pleno',1);
+
+insert into Login (loginUsuario,senhaUsuario,nivelAcesso,fkFuncionario) values
+('admin','admin',1,1),
+('usuario','usuario',2,2);
 
 select * from Login;
 
+insert into Ambiente(descricaoAmbiente,localizacaoAmbiente,fkEmpresa) values
+('Sala dos Devs','3º Andar',1);
+
 select * from Sensor order by idSensor desc;
+
+-- DROPS EM SEQUECIA CORRETA
+-- drop table endereco;
+-- drop table login;
+-- drop table Funcionario;
+-- drop table Funcionamento;
+-- drop table Sensor;
+-- drop table Ambiente;
+-- drop table Empresa;
