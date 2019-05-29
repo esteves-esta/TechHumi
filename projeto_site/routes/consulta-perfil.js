623f2a457f5ec8e410a5c4fbb062f90e9ec836f6
Aluno bandtec <github@alunos.bandtec.com.br>
2019-05-29 20:29:49 -0300

    adicionei info do usuario no sessionStorage @esteves-esta

---
+++ projeto_site/routes/consulta-perfil.js
@@ -0,0 +1,44 @@
+select * from Empresa where idEmpresa = (
+    select fkEmpresa from Funcionario where idFuncionario = 2);
+
+
+    // não mexa nestas 3 linhas!
+var express = require('express');
+var router = express.Router();
+var banco = require('../app-banco');
+// não mexa nessas 3 linhas!
+
+router.post('/entrar', function (req, res, next) {
+
+  banco.conectar().then(() => {
+    console.log(`Chegou p/ login: ${JSON.stringify(req.body)}`);
+    var login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
+    var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login
+    if (login == undefined || senha == undefined) {
+      throw new Error(`Dados de login não chegaram completos: ${login} / ${senha}`);
+    }
+    return banco.sql.query(`select * from Login where loginUsuario='${login}' and senhaUsuario='${senha}'`);
+  }).then(consulta => {
+
+    console.log(`Usuários encontrados: ${JSON.stringify(consulta.recordset)}`);
+
+    if (consulta.recordset.length==1) {
+      res.send(consulta.recordset[0]);
+    } else {
+      res.sendStatus(404);
+    }
+
+  }).catch(err => {
+
+    var erro = `Erro no login: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+// não mexa nesta linha!
+module.exports = router;
