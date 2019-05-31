ae908d024b18eaef9fe5abeb41d153b24485e1ab
Aluno <Aluno@Digital.net>
2019-05-31 20:35:00 -0300

    adicionei script do suporte

---
+++ projeto_site/public/Site_usuario/Usuarios/Representante/js/suporte.js
@@ -0,0 +1,14 @@
+
+// <![CDATA[
+var ttChatLoaderS = document.createElement('script');
+document.tomticketChatLoaderScriptVersion = 2;
+ttChatLoaderS.src = 'https://techhumicorporation.tomticket.com/scripts-chat/chat.min.js'
++ '?id=EP31680'
++ '&account=2027520P25052019035504'
++ '&autoOpen=0'
++ '&hideWhenOffline=0'
++ '&d=techhumicorporation'
++ '&ts=' + new Date().getTime()
++ '&ref=' + encodeURIComponent(document.URL);
+document.body.appendChild(ttChatLoaderS);
+//]]>
