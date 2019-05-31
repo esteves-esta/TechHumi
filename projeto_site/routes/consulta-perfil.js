0100640a48397f1272c68b35c3df88c3f9c9df28
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2019-05-31 08:20:19 -0300

    alterar dados no perfil FUNCIONANDO

--- projeto_site/routes/consulta-perfil.js
+++ projeto_site/routes/consulta-perfil.js
@@ -69,6 +69,110 @@ var endereco = {
   numero: '',
 };
 
+
+//ALTERAÇÃO DO EMPRESA ENDEREÇO E FUNCIONÁRIO
+router.post('/alterar-perfil', function (req, res, next) {
+
+  empresa.codigo = req.body.cd_emp;
+  empresa.nome = req.body.empresa;
+  empresa.cnpj = req.body.cnpj;
+  empresa.telefone_um = req.body.tele1;
+  empresa.telefone_dois = req.body.tele2;
+
+  endereco.codigo = req.body.cd_end;
+  endereco.logradouro = req.body.logradouro;
+  endereco.cep = req.body.cep;
+  endereco.bairro = req.body.bairro;
+  endereco.complemento = req.body.complemento;
+  endereco.referencia = req.body.referencia;
+  endereco.cidade = req.body.cidade;
+  endereco.uf = req.body.uf;
+  endereco.numero = req.body.numero;
+
+  funcionario.codigo = req.body.cd_func;
+  funcionario.nome = req.body.representante;
+  funcionario.rg = req.body.rg;
+  funcionario.cpf = req.body.cpf;
+  funcionario.telefone = req.body.telefone;
+  funcionario.email = req.body.email;
+
+  banco.conectar().then(() => {
+
+    ///ALTERAÇÃO DA EMPRESA
+    banco.sql.query(`update Endereco set 
+         logradouro = '${endereco.logradouro}', 
+         numero = '${endereco.numero}',
+         complemento = '${endereco.complemento}',
+         bairro = '${endereco.bairro}',
+         cidade = '${endereco.cidade}',
+         uf = '${endereco.uf}',
+         cep = '${endereco.cep}',
+         referencia = '${endereco.referencia}'
+         where idEndereco = ${endereco.codigo};`)
+      .then(function () {
+        console.log('Endereço alterado com sucesso');
+        res.sendStatus(201);
+
+      }).catch(err => {
+
+        var erro = `Erro na alteração: ${err}`;
+        console.error(erro);
+        res.status(500).send(erro);
+
+      }).finally(() => {
+
+          banco.sql.query(`update Funcionario set 
+          nomeFuncionario = '${funcionario.nome}', 
+          rgFuncionario = '${funcionario.rg}',
+          cpfFuncionario = '${funcionario.cpf}',
+          emailFuncionario = '${funcionario.email}',
+          telefoneFuncionario = '${funcionario.telefone}'
+          where idFuncionario = ${funcionario.codigo};`)
+      
+        .then(consulta => {
+          console.log('alterou');
+          res.sendStatus(201);
+      
+        }).catch(err => {
+      
+          var erro = `Erro: ${err}`;
+          console.error(erro);
+      
+          res.status(500).send(erro);
+      
+        }).finally(() => {
+          banco.sql.close();
+        });
+      });
+
+
+    return banco.sql.query(`update Empresa set 
+      nomeEmpresa = '${empresa.nome}', 
+      cnpjEmpresa = '${empresa.cnpj}',
+      telefoneEmpresa1 = '${empresa.telefone_um}',
+      telefoneEmpresa2 = '${empresa.telefone_dois}'
+      where idEmpresa = ${empresa.codigo};`);
+
+  }).then(consulta => {
+    console.log('Empresa alterada com sucesso!');
+    res.sendStatus(201);
+
+  }).catch(err => {
+
+    var erro = `Erro: ${err}`;
+    console.error(erro);
+
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+
+
+
 //ALTERAÇÃO DO EMPRESA
 router.post('/alterar-empresa', function (req, res, next) {
 
@@ -121,8 +225,8 @@ router.post('/alterar-empresa', function (req, res, next) {
     return banco.sql.query(`update Empresa set 
       nomeEmpresa = '${empresa.nome}', 
       cnpjEmpresa = '${empresa.cnpj}',
-      telefoneEmpresa1 = '${empresa.telefone_dois}',
-      telefoneEmpresa2 = '${empresa.telefone_um}'
+      telefoneEmpresa1 = '${empresa.telefone_um}',
+      telefoneEmpresa2 = '${empresa.telefone_dois}'
       where idEmpresa = ${empresa.codigo};`);
 
   }).then(consulta => {
