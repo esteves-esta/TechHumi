7191e34180d9c4e6acaae5337957f59785829f27
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-06-06 16:16:04 -0300

    Enviando email da pag institucional @1506kidrauhl

--- projeto_site/routes/email.js
+++ projeto_site/routes/email.js
@@ -45,6 +45,50 @@ const transporter = nodemailer.createTransport({
   });
 });
 
+
+// ENVIAR EMAIL EMPRESA
+router.post('/emailEmp', function (req, res, next) {
+
+  // dados da TechHumi q vai enviar
+  const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+          user: "techhumicorporation@gmail.com",
+          pass: "Voupassar123"
+      },
+      tls: { rejectUnauthorized: false }
+    });
+  
+    // dados do usuario q vamos enviar
+    var email="techhumicorporation@gmail.com"; // email do usuario
+    const mailOptions = {// email q será enviado
+      from: 'techhumicorporation@gmail.com',
+      to: email,
+      subject: 'Contato com a techHumi',
+      html: `<h1>Dados do interessado</h1> <br>
+      Empresa: ${req.body.nomeEmp} <br>
+      Representante: ${req.body.nomeRep} <br>
+      CNPJ: ${req.body.cnpjEmp} <br>
+      Telefone: (${req.body.dd})${req.body.telEmp}<br>
+      Mensagem:${req.body.msg}`
+    };
+  
+    // Autentifica se foi enviado e envia notificação
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+        res.status(500).send(error);
+  
+      } else {
+        console.log('Email enviado: ' + info.response);
+        res.send(info.response);
+      }
+    });
+  });
+  
+
   // não mexa nesta linha!
 module.exports = router;
 
