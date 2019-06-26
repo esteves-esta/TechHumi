c47db32e5f3301d39aad4e4ebf95b60e87e06964
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2019-06-25 22:14:32 -0300

    alterei tela de historico

--- projeto_site/public/Site_usuario/Usuarios/theme/js/con-historico.js
+++ projeto_site/public/Site_usuario/Usuarios/theme/js/con-historico.js
@@ -4,81 +4,185 @@
 autentificar('funcionario');
 // ---------------------------------------------------------------
 
+
+
 //chama a atualização dos dois graficos no onload
 function carregar_grafico() {
     atualizarGrafico();
-    setTimeout(consultar_dados(), 10000);
+    consultar_dados();
 }
+
+// ---------------------------------------------------------------
+// DECLARAR VÁRIAVEIS
+var exibiu_grafico = false;
+var lista_dados = [];
+
+
+// ---------------------------------------------------------------
+// DECLARAR FUNÇÕES
+
+// altere aqui as configurações do gráfico
+// (tamanhos, cores, textos, etc)
+function configurarGrafico() {
+    var configuracoes = {
+        responsive: true,
+        maintainAspectRatio: false,
+        animation: exibiu_grafico ? false : {
+            duration: 1000
+        },
+        hoverMode: 'index',
+        stacked: false,
+        title: {
+            display: true,
+            text: 'Histórico recente de temperatura e umidade'
+        },
+        scales: {
+            yAxes: [{
+                type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                display: true,
+                position: 'left',
+                id: 'y-temperatura',
+            }, {
+                type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                display: true,
+                position: 'right',
+                id: 'y-umidade',
+
+                // grid line settings
+                gridLines: {
+                    drawOnChartArea: false, // only want the grid lines for one axis to show up
+                },
+            }],
+        }
+    };
+
+    exibiu_grafico = true;
+
+    return configuracoes;
+}
+
+// ---------------------------------------------------------------
+
+
+// altere aqui como os dados serão exibidos
+// e como são recuperados do BackEnd
+function obterDadosGrafico() {
+
+    // neste JSON tem que ser 'labels', 'datasets' etc, 
+    // porque é o padrão do Chart.js
+    var dados = {
+        labels: [],
+        datasets: [{
+            yAxisID: 'y-temperatura',
+            label: 'Temperatura',
+            borderColor: "#ff16b9",
+            backgroundColor: "#ff16b9",
+            fill: false,
+            data: []
+        },
+        {
+            yAxisID: 'y-umidade',
+            label: 'Umidade',
+            borderColor: "#16abff",
+            backgroundColor: "#16abff",
+            fill: false,
+            data: []
+        }
+        ]
+    };
+
+    fetch('/leituras/ultimas', {
+        cache: 'no-store'
+    }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+
+                // console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+
+                resposta.reverse();
+
+                for (i = 0; i < resposta.length; i++) {
+                    var registro = resposta[i];
+
+                    // aqui, após 'registro.' use os nomes 
+                    // dos atributos que vem no JSON 
+                    // que gerou na consulta ao banco de dados
+
+                    dados.labels.push(registro.momento);
+
+                    dados.datasets[0].data.push(registro.temperatura);
+                    dados.datasets[1].data.push(registro.umidade);
+
+                    lista_dados.push(registro);
+                }
+                // console.log(JSON.stringify(dados));
+
+                div_aguarde.style.display = 'none';
+
+                plotarGrafico(dados);
+                consultar_dados();
+            });
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    }).catch(function (error) {
+        obterDadosGrafico();
+        console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+    });
+
+}
+
+
+
+// ---------------------------------------------------------------
+
+// só altere aqui se souber o que está fazendo!
+function plotarGrafico(dados) {
+    var ctx = canvas_grafico.getContext('2d');
+    window.grafico_linha = Chart.Line(ctx, {
+        data: dados,
+        options: configurarGrafico()
+    });
+}
+
+// ---------------------------------------------------------------
+// só mexer se quiser alterar o tempo de atualização
+// ou se souber o que está fazendo!
+function atualizarGrafico() {
+    obterDadosGrafico();
+}
+
 
 
 // ---------------------------------------------------------------
 // DECLARAR FUNÇÕES
 
 function consultar_dados() {
-    // guarda código do usuário que está no atributo usuario_bandtec
-    // em um json e depois utiliza a classe URLSearchParams
-    //para mandar para o arquivo j
-    var cdEmpresa = { codigo: sessionStorage.idEmpresa }
-    var corpo = new URLSearchParams(cdEmpresa);
 
-    fetch("/consulta/consulta-historico", {
-        method: "POST",
-        body: corpo
-    }).then(function (response) {
-        
-        if (response.ok) {
+    var conteudo = '';
+    if (lista_dados.length != 0) {
+        for (r = 0; r < lista_dados.length; r++) {
+            var atual = lista_dados[r];
 
-            response.json().then(function (resposta) {
-                if (resposta.length == undefined) {
-
-                    swal({
-                        title: "Não foram registrados dados.",
-                        text: "   ",
-                        icon: "info",
-                        buttons: false,
-                        timer: 2500,
-                        closeOnClickOutside: true,
-                    });
-                }
-                else {
-                    // devolve informação da empresa
-                    var conteudo = '';
-                    for (r = 0; r < resposta.length; r++) {
-                        var atual = resposta[r];
-
-                        conteudo += `<tr>
+            conteudo += `<tr>
                         <td>${atual.data}</td>
-                        <td>${atual.hora}</td>
+                        <td>${atual.momento}</td>
                         <td>${atual.temperatura}</td>
                         <td>${atual.umidade}</td> 
                     </tr>`;
-                    }
-
-                    corpo_tabela.innerHTML = conteudo;
-                    cabeca_tabela.style.display = "";
-
-                    $('#tabela').dataTable({
-                        "language": {
-                            "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Portuguese-Brasil.json"
-                        }
-                      });
-                    $('input[type="search"').attr('id', 'search');
-
-                }
-               
-            });
-        } else {
-            //consultar_dados();
-            
         }
-    }).catch(() => {
-        // setInterval(consultar_dados(), 100);
-        //consultar_dados()
-        
-    });
 
-    return false;
+        corpo_tabela.innerHTML = conteudo;
+
+
+        $('#tabela').dataTable({
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Portuguese-Brasil.json"
+            }
+        });
+    }
 }
+
 
 
 
